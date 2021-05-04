177c00615de01d40241d237adbbb4dab8ec312b0
Iveta <quietbits@gmail.com>
2021-05-03 21:55:52 -0400

    Mostly works

---
+++ packages/sds-web/src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@stellar/design-system";
+
+export const App = () => (
+  <div>
+    SDS WEBSITE <Button>Test</Button>
+  </div>
+);
--- packages/sds/webpack.config.js
+++ packages/sds/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     libraryTarget: "commonjs-module",
   },
   resolve: {
