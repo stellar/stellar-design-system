735fa71ab0e04ae547d8e881fb6dca60dc214c88
Iveta <quietbits@gmail.com>
2021-05-06 15:09:55 -0400

    Button component updated + button examples

--- @stellar/design-system-website/src/App.tsx
+++ @stellar/design-system-website/src/App.tsx
@@ -1,8 +1,88 @@
-import { Button } from "@stellar/design-system";
+import {
+  Button,
+  IconCheckmark,
+  Heading2,
+  Heading3,
+} from "@stellar/design-system";
+
+import "styles.scss";
 
 export const App = () => (
-  <div>
-    SDS WEBSITE
-    <Button>Test</Button>
-  </div>
+  <>
+    <div className="inset">
+      <Heading2>Button</Heading2>
+
+      <Heading3>Primary</Heading3>
+      <div className="item-container">
+        <div className="item-wrapper">
+          <Button>Label</Button>
+        </div>
+        <div className="item-wrapper">
+          <Button disabled>Label</Button>
+        </div>
+        <div className="item-wrapper">
+          <Button iconLeft={<IconCheckmark />}>Label</Button>
+        </div>
+        <div className="item-wrapper">
+          <Button iconLeft={<IconCheckmark />} disabled>
+            Label
+          </Button>
+        </div>
+        <div className="item-wrapper">
+          <Button iconRight={<IconCheckmark />}>Label</Button>
+        </div>
+        <div className="item-wrapper">
+          <Button iconRight={<IconCheckmark />} disabled>
+            Label
+          </Button>
+        </div>
+      </div>
+
+      <Heading3>Secondary</Heading3>
+      <div className="item-container">
+        <div className="item-wrapper">
+          <Button variant={Button.variant.secondary}>Label</Button>
+        </div>
+        <div className="item-wrapper">
+          <Button variant={Button.variant.secondary} disabled>
+            Label
+          </Button>
+        </div>
+        <div className="item-wrapper">
+          <Button
+            variant={Button.variant.secondary}
+            iconLeft={<IconCheckmark />}
+          >
+            Label
+          </Button>
+        </div>
+        <div className="item-wrapper">
+          <Button
+            variant={Button.variant.secondary}
+            iconLeft={<IconCheckmark />}
+            disabled
+          >
+            Label
+          </Button>
+        </div>
+        <div className="item-wrapper">
+          <Button
+            variant={Button.variant.secondary}
+            iconRight={<IconCheckmark />}
+          >
+            Label
+          </Button>
+        </div>
+        <div className="item-wrapper">
+          <Button
+            variant={Button.variant.secondary}
+            iconRight={<IconCheckmark />}
+            disabled
+          >
+            Label
+          </Button>
+        </div>
+      </div>
+    </div>
+  </>
 );
--- @stellar/design-system/src/components/Button/index.tsx
+++ @stellar/design-system/src/components/Button/index.tsx
@@ -1,25 +1,52 @@
 import React from "react";
 import "./styles.scss";
 
-export enum ButtonVariant {
+enum ButtonVariant {
   primary = "primary",
   secondary = "secondary",
 }
 
+enum ButtonIconPosition {
+  left = "left",
+  right = "right",
+}
+
+interface ButtonComponent {
+  variant: typeof ButtonVariant;
+}
+
+interface ButtonIconProps {
+  position: ButtonIconPosition;
+  children: React.ReactNode;
+}
+
+const ButtonIcon = ({ position, children }: ButtonIconProps) => (
+  <span className={`ButtonIcon ButtonIcon--${position}`}>{children}</span>
+);
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  icon?: React.ReactNode;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
   variant?: ButtonVariant;
   children: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  icon,
+export const Button: React.FC<ButtonProps> & ButtonComponent = ({
+  iconLeft,
+  iconRight,
   variant = ButtonVariant.primary,
   children,
   ...props
 }) => (
-  <button className="Button" data-variant={variant} {...props}>
-    {icon && <span className="ButtonIcon">{icon}</span>}
+  <button className={`Button Button--${variant}`} {...props}>
+    {iconLeft ? (
+      <ButtonIcon position={ButtonIconPosition.left}>{iconLeft}</ButtonIcon>
+    ) : null}
     {children}
+    {iconRight ? (
+      <ButtonIcon position={ButtonIconPosition.right}>{iconRight}</ButtonIcon>
+    ) : null}
   </button>
 );
+
+Button.variant = ButtonVariant;
