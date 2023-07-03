0f1a58d98c25d859bf34c8357d55cd62cab4955d
Iveta <quietbits@gmail.com>
2023-07-03 15:49:40 -0400

    Fix broken link

--- @stellar/design-system-website/docusaurus.config.js
+++ @stellar/design-system-website/docusaurus.config.js
@@ -79,15 +79,6 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
             title: "More",
             items: [
               {
