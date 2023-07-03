59fb05d503d9f79f2050536a4e060bc7978caac6
Iveta <quietbits@gmail.com>
2023-07-03 11:13:05 -0400

    Basic Docusaurus setup

--- @stellar/design-system-website/babel.config.js
+++ @stellar/design-system-website/babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: [
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          browsers: "last 2 versions",
-        },
-        modules: false,
-        loose: false,
-      },
-    ],
-  ],
-  plugins: ["transform-class-properties", "react-hot-loader/babel"],
-  env: {
-    test: {
-      plugins: ["transform-es2015-modules-commonjs"],
-    },
-  },
+  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
 };
