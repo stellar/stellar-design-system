25d30f28cb39f09eb6974c78869787fa79472559
Iveta <quietbits@gmail.com>
2023-07-06 09:50:50 -0400

    Temp rename

--- @stellar/design-system-website/babel.config.js
+++ @stellar/design-system-website/babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+  presets: [
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: "last 2 versions",
+        },
+        modules: false,
+        loose: false,
+      },
+    ],
+  ],
+  plugins: ["transform-class-properties", "react-hot-loader/babel"],
+  env: {
+    test: {
+      plugins: ["transform-es2015-modules-commonjs"],
+    },
+  },
 };
--- @stellar/design-system-website__delete/README.md
+++
@@ -1,3 +0,0 @@
-# Stellar Design System Website
-
-TODO
