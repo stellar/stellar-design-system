496aac5631172d5d2a7a0dd1160f3776bfb5ab33
Iveta <quietbits@gmail.com>
2023-10-05 16:03:10 -0400

    Icon and Logo docs + preview

--- @stellar/design-system-website/src/theme/MDXComponents.js
+++ @stellar/design-system-website/src/theme/MDXComponents.js
@@ -2,10 +2,12 @@ import MDXComponents from "@theme-original/MDXComponents";
 import { ComponentDescription } from "@site/src/components/ComponentDescription";
 import { ComponentProps } from "@site/src/components/ComponentProps";
 import { PreviewBlock } from "@site/src/components/PreviewBlock";
+import { AssetPreview } from "@site/src/components/AssetPreview";
 
 export default {
   ...MDXComponents,
   ComponentDescription,
   ComponentProps,
   PreviewBlock,
+  AssetPreview,
 };
