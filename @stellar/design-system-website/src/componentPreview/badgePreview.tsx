17e79e401c040d9ec0201e62612f769fc5db9ffe
Iveta <quietbits@gmail.com>
2024-01-17 16:33:51 -0500

    Badge

--- @stellar/design-system-website/src/componentPreview/badgePreview.tsx
+++ @stellar/design-system-website/src/componentPreview/badgePreview.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { ComponentPreview } from "@site/src/components/PreviewBlock";
+import { Icon } from "@stellar/design-system";
 
 export const badgePreview: ComponentPreview = {
   options: [
@@ -7,8 +9,16 @@ export const badgePreview: ComponentPreview = {
       prop: "variant",
       options: [
         {
-          value: "default",
-          label: "Default",
+          value: "primary",
+          label: "Primary",
+        },
+        {
+          value: "secondary",
+          label: "Secondary",
+        },
+        {
+          value: "tertiary",
+          label: "Tertiary",
         },
         {
           value: "success",
@@ -19,12 +29,61 @@ export const badgePreview: ComponentPreview = {
           label: "Warning",
         },
         {
-          value: "pending",
-          label: "Pending",
-        },
-        {
           value: "error",
           label: "Error",
+        },
+      ],
+    },
+    {
+      type: "select",
+      prop: "size",
+      options: [
+        {
+          value: "sm",
+          label: "SM",
+        },
+        {
+          value: "md",
+          label: "MD",
+        },
+        {
+          value: "lg",
+          label: "LG",
+        },
+      ],
+    },
+    {
+      type: "checkbox",
+      prop: "isOutlined",
+      label: "Outlined",
+    },
+    {
+      type: "checkbox",
+      prop: "isSquare",
+      label: "Square",
+    },
+    {
+      type: "checkbox",
+      prop: "isStatus",
+      label: "Status",
+    },
+    {
+      type: "select",
+      prop: "icon",
+      customValue: <Icon.ArrowNarrowRight />,
+      clearProp: "iconPosition",
+      options: [
+        {
+          value: "",
+          label: "No icon",
+        },
+        {
+          value: '{"iconPosition": "right"}',
+          label: "Icon right",
+        },
+        {
+          value: '{"iconPosition": "left"}',
+          label: "Icon left",
         },
       ],
     },
