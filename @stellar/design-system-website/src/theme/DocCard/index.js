54f17ef0a0989a9a029cae76e90fa8e222f3984d
Iveta <quietbits@gmail.com>
2024-09-27 10:03:21 -0400

    Cleanup

--- @stellar/design-system-website/src/theme/DocCard/index.js
+++ @stellar/design-system-website/src/theme/DocCard/index.js
@@ -71,7 +71,7 @@ function CardCategory({ item }) {
   return (
     <CardLayout
       href={href}
-      icon="🗃️"
+      icon="null"
       title={item.label}
       description={item.description ?? categoryItemsPlural(item.items.length)}
     />
